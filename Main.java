d610e5a15bffe65aee49325b347f372eb65537fd
ryanm100 <ryan.milton@patelhs.org>
2021-02-23 15:01:11 +0000

    Added comments, going to start the method now so it's done by tonight.

--- Main.java
+++ Main.java
@@ -1,8 +1,19 @@
 import java.util.Scanner; //Including scanner
 class Main {
   public static void main(String[] args) {
+{
+  Scanner scan = new Scanner(System.in); //Scanner statement
+  System.out.println("Are you a student or a teacher?");
+  String userType = scan.next(); 
+  System.out.println("You are a " + userType + " right?");
+  String userConfirm = scan.next();
+  System.out.println("Good! Welcome to the Q/A!");
 
-  Scanner scan = new Scanner(System.in);
-    
-  }
+  //Above is a little QA, so that we can just get some information, and I will be adding a couple of more questions, like how many people are in your school or what, and how many classrooms/teachers are there. 
+
+  //Method 1 for the recursive
 }
+
+    
+    }
+  }
