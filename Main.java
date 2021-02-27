5ea0b8c568edc44903fe67e16b2aa6a5fbe86c60
Ryan <ryan.milton@patelhs.org>
2021-02-27 03:12:22 +0000

    Fixed base case added comments

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 import java.util.Scanner; //Including scanner
 class Main {
   public static void main(String[] args) {
+
+    //list of class grades
     int[] grades = {22,39,54,9,42,29,51,93,37,91,27,48,83,39,69};
 
     int bestGrade = highestGrade(0, grades[0], grades);
@@ -9,12 +11,17 @@ class Main {
   }
 
   static int highestGrade(int index, int highestGrade, int[] grades) {
-    if(grades.length-1 == index) {
+    //base case
+
+    //
+    if(grades.length == index) {
       return highestGrade;
     } else {
+
       if(grades[index] > highestGrade) {
         highestGrade = grades[index];
       }
+      // recursion
       return highestGrade(index + 1, highestGrade, grades);
     }
   }
